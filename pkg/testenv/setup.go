d14ab64f9e54583050acaadaa4129dbb2e60c664
debsmita1 <debsmita.santra@gmail.com>
2022-07-04 13:09:32 +0530

    fix logic to handle directories

--- pkg/testenv/setup.go
+++ pkg/testenv/setup.go
@@ -19,7 +19,7 @@ func isDirectory(path string) bool {
 	return fileInfo.IsDir()
 }
 
-func organizeFiles(files []string, files1 []string, files2 []string) (crdFiles []string, resourceWatcherFiles []string) {
+func organizeFiles(files []string, crdf []string, rwf []string) (crdFiles []string, resourceWatcherFiles []string) {
 	for _, filename := range files {
 		if filename == "" {
 			continue
@@ -27,10 +27,10 @@ func organizeFiles(files []string, files1 []string, files2 []string) (crdFiles [
 		lowercaseFilename := strings.ToLower(filename)
 		if strings.HasSuffix(lowercaseFilename, ".yaml") || strings.HasSuffix(lowercaseFilename, ".yml") {
 			klog.Infof(" - %s\n", filename)
-			files1 = append(files1, filename)
+			crdf = append(crdf, filename)
 		} else if strings.HasSuffix(lowercaseFilename, ".js") || strings.HasSuffix(lowercaseFilename, ".ts") {
 			klog.Infof(" - %s\n", filename)
-			files2 = append(files2, filename)
+			rwf = append(rwf, filename)
 		} else if isDirectory(filename) {
 			files, err := ioutil.ReadDir(filename)
 			if err != nil {
@@ -38,15 +38,16 @@ func organizeFiles(files []string, files1 []string, files2 []string) (crdFiles [
 			}
 			temp := []string{}
 			for _, f := range files {
-				temp = append(temp, f.Name())
+				temp = append(temp, fmt.Sprintf("%s/%s", filename,
+					f.Name()))
 			}
-			return organizeFiles(temp, files1, files2)
+			return organizeFiles(temp, crdf, rwf)
 		} else {
 			klog.Fatalf("Unsupported test environment configuration: %s", filename)
 			os.Exit(1)
 		}
 	}
-	return files1, files2
+	return crdf, rwf
 }
 
 func Setup(files []string, flags *flag.FlagSet) {
